c1d832aa9210bb13727c31a267c183008d74e560
Shakir <shakirkhan72727@gmail.com>
2025-09-18 21:22:53 +0500

    initial backend setup

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
