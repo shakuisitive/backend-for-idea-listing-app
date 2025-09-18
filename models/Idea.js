b6d71dc26c6161ea346954791b85df85d76a688d
Shakir <shakirkhan72727@gmail.com>
2025-09-19 02:08:24 +0500

    set up controllers to get ideas

---
+++ models/Idea.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const ideaSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    summary: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    tags: [String],
+    default: [],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Idea = mongoose.model("Idea", ideaSchema);
+
+export default Idea;
--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -1,20 +1,41 @@
 import express from "express";
+import mongoose from "mongoose";
+import Idea from "../models/Idea.js";
 const router = express.Router();
 
 router
   .route("/")
-  .get((req, res) => {
-    const ideas = [
-      { id: 1, title: "idea 1", description: "this is idea 1" },
-      { id: 1, title: "idea 2", description: "this is idea 2" },
-      { id: 1, title: "idea 3", description: "this is idea 3" },
-    ];
-
-    res.status(201).json(ideas);
+  .get(async (req, res, next) => {
+    try {
+      const ideas = await Idea.find();
+      res.status(200).json(ideas);
+    } catch (error) {
+      next(error);
+    }
   })
   .post((req, res) => {
     const { title, description } = req.body;
     res.send({ title, description });
   });
+
+router.route("/:id").get(async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(404);
+      throw new Error("Invalid Id");
+    }
+
+    const idea = await Idea.findById(id);
+    if (!idea) {
+      res.status(404);
+      throw new Error("Idea Not Found");
+    }
+    res.json(idea);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
