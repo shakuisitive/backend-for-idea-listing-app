5e7eb4cb0d1a4b155e4820205fe7e10d932d3253
Shakir <shakirkhan72727@gmail.com>
2025-09-20 18:21:40 +0500

    implement login feature

--- models/User.js
+++ models/User.js
@@ -34,6 +34,11 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+// Compare the entered password with the hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 import { generateToken } from "../utils/generateToken.js";
 
@@ -44,7 +45,53 @@ router.route("/register").post(async (req, res, next) => {
       },
     });
   } catch (err) {
-    console.log(err);
+    next(err);
+  }
+});
+
+router.route("/login").post(async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("Email and password are required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(401);
+      throw new Error("Invalid credentials ");
+    }
+
+    if (!(await user.matchPassword(password))) {
+      res.status(401);
+      throw new Error("Invalid credentials");
+    }
+
+    // Create Tokens
+    const payload = { userId: user._id.toString() };
+    const accessToken = await generateToken(payload, "1m");
+    const refreshToken = await generateToken(payload, "30d");
+
+    // Set refresh token in HTTP-Only cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+    });
+
+    res.status(201).json({
+      accessToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
     next(err);
   }
 });
